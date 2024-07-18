3d1936ccd59d4875093a17ca3f9cdf20a07c5013
jaortizlamas <57230258+jaortizlamas@users.noreply.github.com>
2024-07-18 18:06:39 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,119 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+data "aws_subnet" "az_a" {
+    availability_zone = "eu-west-1a"
+}
+
+data "aws_subnet" "az_b" {
+    availability_zone = "eu-west-1b"
+}
+resource "aws_instance" "mi_servidor_1" {
+    ami = "ami-0aef57767f5404a3c"
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.az_a.id
+    vpc_security_group_ids = [  aws_security_group.mi_sg.id]
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hola desde server 1!" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+    tags = {
+        Name = "mi_server_1"
+    }
+}
+
+resource "aws_instance" "mi_servidor_2" {
+    ami = "ami-0aef57767f5404a3c"
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.az_b.id
+    vpc_security_group_ids = [  aws_security_group.mi_sg.id]
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hola desde server 2!" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+    tags = {
+        Name = "mi_server_2"
+    }
+}
+resource "aws_security_group" "mi_sg" {
+    name = "mi_servidor_sg"
+
+    ingress {
+        security_groups = [aws_security_group.alb.id]
+        #cidr_blocks = ["0.0.0.0/0"]
+        description = "Acceso alpuerto 8080"
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+    }
+}
+
+resource "aws_lb" "alb" {
+    load_balancer_type = "application"
+    name = "terraform-alb"
+    security_groups = [aws_security_group.alb.id]
+    subnets = [data.aws_subnet.az_a.id, data.aws_subnet.az_b.id]
+}
+
+resource "aws_security_group" "alb" {
+    name = "malb-sg"
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Acceso alpuerto 80"
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+    }
+    egress {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Acceso alpuerto 8080 de las instancias"
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+    }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+resource "aws_lb_target_group" "this" {
+    name = "terraforms-alb-target-group"
+    port = 80
+    vpc_id = data.aws_vpc.default.id
+    protocol = "HTTP"
+
+    health_check {
+        enabled = true
+        matcher = "200"
+        path = "/"
+        port = "8080"
+        protocol = "HTTP"
+    }
+}
+
+resource "aws_lb_target_group_attachment"  "mi_servidor_1"{
+    target_group_arn = aws_lb_target_group.this.arn
+    target_id = aws_instance.mi_servidor_1.id
+    port = 8080
+}
+
+resource "aws_lb_target_group_attachment"  "mi_servidor_2"{
+    target_group_arn = aws_lb_target_group.this.arn
+    target_id = aws_instance.mi_servidor_2.id
+    port = 8080
+}
+
+resource "aws_lb_listener" "this" {
+    load_balancer_arn = aws_lb.alb.arn
+    port = 80
+    protocol = "HTTP"
+    default_action {
+        target_group_arn = aws_lb_target_group.this.arn
+        type = "forward"
+    }
+}

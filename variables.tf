63df72b449389705107a4f69202337fe3bb4fb77
284028 <jose.angel.ortiz.lamas@gmail.com>
2024-07-19 10:46:37 +0200

    introducidos valores por defecto

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "puerto_servidor" {
   description = "puerto de las instancias"
   type = number
+  default = 8080
 }
 variable "puerto_lb" {
   description = "puerto del lb"
   type = number
+  default = 80
 }
 variable "tipo_instancia" {
   description = "tipo de instancia en AWS"
   type = string
+  default = "t2.micro"
 }

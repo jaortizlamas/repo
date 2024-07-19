de7238b59fcf68c8b216c72c62a00f67bee170bb
284028 <jose.angel.ortiz.lamas@gmail.com>
2024-07-19 11:35:47 +0200

    cambio a any

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "puerto_servidor" {
   description = "puerto de las instancias"
-  type = number
+  type = any
   default = 8080
 }
 variable "puerto_lb" {
   description = "puerto del lb"
-  type = number
+  type = any
   default = 80
 }
 variable "tipo_instancia" {
   description = "tipo de instancia en AWS"
-  type = string
+  type = any
   default = "t2.micro"
 }

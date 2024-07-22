6f423bc8fcf9176e9a27d9f16382f4a49866224d
284028 <jose.angel.ortiz.lamas@gmail.com>
2024-07-22 16:18:54 +0200

    s3

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
     provider "aws" {
         region = "eu-west-1"
     }
-
+terraform {
+    backend "s3" {
+        bucket = "terraformjaortiz"
+        key    = "terraformjaortiz/terraform.tfstate"
+        region = "eu-west-1"
+        encrypt = true
+  }
+}
 data "aws_subnet" "az_a" {
     availability_zone = "eu-west-1a"
 }

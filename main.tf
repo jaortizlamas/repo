fbc7d00c2abd35f5377b0642c801d2d576c5b98b
284028 <jose.angel.ortiz.lamas@gmail.com>
2024-07-22 16:06:16 +0200

    cambios

--- main.tf
+++ main.tf
@@ -3,14 +3,7 @@
     provider "aws" {
     region = "eu-west-1"
 }
-terraform {
-    backend "s3" {
-        bucket = "terraformjaortiz"
-        key    = "terraformjaortiz/terraform.tfstate"
-        region = "eu-west-1"
-        encrypt = true
-  }
-}
+
 
 
 data "aws_subnet" "az_a" {
